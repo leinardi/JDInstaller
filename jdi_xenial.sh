5740b542447e74056f06fcaeb2088d20ed163e00
Roberto Leinardi <leinardi@gmail.com>
2017-07-15 18:29:32 +0200

    Translated to English

--- jdi_xenial.sh
+++ jdi_xenial.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 # JDInstall
-# TODO:
-# *) Scrivere una breve descrizione del programma al posto del commento #JDInstall
-# *) Installazione selettiva dei repository esterni e delle applicazioni a essi legate
-# *) Prevedere un "dpkg --config -a" in caso di problemi
-#
-#
-DISTRIBUZIONE="xenial"
-VERSIONE="JDInstall 1.1510.0 ($DISTRIBUZIONE)"
 
-APPZ_REPO_UFFICIALI="alacarte autofs build-essential bwm-ng calibre cmatrix compizconfig-settings-manager conky-all curl dconf-tools default-jdk dkms exfat-fuse exfat-utils faenza-icon-theme filezilla finch flashplugin-installer gdebi gimp git gnome-backgrounds gnome-commander gnome-gmail gnome-mplayer gnome-search-tool gnome-system-tools gnome-themes-standard gnome-tweak-tool gparted gthumb gtk-recordmydesktop gwakeonlan hardinfo hddtemp htop icedtea-plugin indicator-multiload inkscape inxi iotop jstest-gtk k3b libreoffice libsoil1 lm-sensors linux-headers-generic mc meld mercurial mplayer mumble nautilus-actions nautilus-dropbox nautilus-image-converter ncdu nfs-common nfs-kernel-server openjdk-8-jdk p7zip p7zip-full p7zip-rar pidgin powertop pavucontrol ppa-purge python-pip python-dev pwgen rar saidar silversearcher-ag sloccount skype smartmontools soundconverter sox subtitleeditor subversion synaptic synapse tagtool tig tofrodos tree ttf-mscorefonts-installer ubuntu-restricted-extras unity-tweak-tool unshield vdpau-va-driver vim vim-gtk vlc wakeonlan zlib1g-dev" #g15daemon tvtime me-tv eclipse texmaker texlive" #myunity ffmpeg synapse
+CODENAME="xenial"
+RELEASE="16.04"
+VERSION="JDInstall $CODENAME"
+PINGSITE="www.google.com"
 
-APPZ_ALTRI_REPO="libdvdcss2 google-chrome-beta" # google-musicmanager-beta google-talkplugin indicator-sensors jdownloader  my-weather-indicator 
+APP_OFFICIAL_REPOS="alacarte autofs build-essential bwm-ng calibre cmatrix compizconfig-settings-manager conky-all curl dconf-tools default-jdk dkms exfat-fuse exfat-utils faenza-icon-theme filezilla finch flashplugin-installer gdebi gimp git gnome-backgrounds gnome-commander gnome-gmail gnome-mplayer gnome-search-tool gnome-system-tools gnome-themes-standard gnome-tweak-tool gparted gthumb gtk-recordmydesktop gwakeonlan hardinfo hddtemp htop icedtea-plugin indicator-multiload inkscape inxi iotop jstest-gtk k3b libreoffice libsoil1 lm-sensors linux-headers-generic mc meld mercurial mplayer mumble nautilus-actions nautilus-dropbox nautilus-image-converter ncdu nfs-common nfs-kernel-server openjdk-8-jdk p7zip p7zip-full p7zip-rar pidgin powertop pavucontrol ppa-purge python-pip python-dev pwgen rar saidar silversearcher-ag sloccount skype smartmontools soundconverter sox subtitleeditor subversion synaptic synapse tagtool tig tofrodos tree ttf-mscorefonts-installer ubuntu-restricted-extras unity-tweak-tool unshield vdpau-va-driver vim vim-gtk vlc wakeonlan zlib1g-dev" #g15daemon tvtime me-tv eclipse texmaker texlive" #myunity ffmpeg synapse
 
-APPZ_DIPENDENZE_ECLIPSE="ant ant-optional aspectj binfmt-support fastjar jarwrapper junit junit4 sat4j x11proto-core-dev x11proto-input-dev x11proto-kb-dev xorg-sgml-doctools xtrans-dev"
+APP_OTHER_REPOS="libdvdcss2 google-chrome-beta" # google-musicmanager-beta google-talkplugin indicator-sensors jdownloader  my-weather-indicator 
 
-APPZ_DIPENDENZE_QT="mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev"
 
-if [[ `uname -m` == 'amd64' || `uname -m` == 'x86_64' ]]
-then
-	APPZ_DIPENDENZE_QT=$APPZ_DIPENDENZE_QT" g++-multilib"
-	
-fi
+#if [[ `uname -m` == 'amd64' || `uname -m` == 'x86_64' ]]
+#then
+	#APP_OTHER_REPOS=$APP_OTHER_REPOS" g++-multilib"	
+#fi
 
-APPZ="$APPZ_REPO_UFFICIALI $APPZ_ALTRI_REPO $APPZ_DIPENDENZE_ECLIPSE $APPZ_DIPENDENZE_QT"
+APPZ="$APP_OFFICIAL_REPOS $APP_OTHER_REPOS"
 
 AUTOYES="--assume-yes"
 
@@ -37,45 +30,43 @@ case $TMPDIR in
 		;;
 esac
 
-while getopts :ahvy OPZIONE
+while getopts :ahvy PARAMETER
 do
-	case $OPZIONE in
+	case $PARAMETER in
 		a)
-		echo "Elenco dei pacchetti da installare:"
+		echo "List of the packages to install:"
 		echo $APPZ
 		exit 0
 		;;
 		h)
-		echo "Uso: $0 [OPZIONE]"
-		echo "Se avviato senza opzioni lo script procede con l'installazione."
-		echo "   -a		Visualizza l'elenco dei pacchetti da installare"
-		echo "   -h		Mostra questo aiuto"
-		echo "   -v		Visualizza il numero di versione"
+		echo "Uso: $0 [PARAMETER]"
+		echo "If no parameter is specified, the script start the installation"
+		echo "   -a		List of the packages to install"
+		echo "   -h		Show this help text"
+		echo "   -v		Show the version"
 		echo ""
-		echo "Segnalare i bug a <webmaster@jdsblog.it>."
-		echo "Maggiori info su http://www.jdsblog.it/jdinstaller/"
+		echo "Issue tracker: https://github.com/leinardi/JDInstaller/issues"
 		exit 0
 		;;	
 		v)
-		echo $VERSIONE
+		echo $VERSION
 		exit 0
 		;;
-		y)
-		$AUTOYES="--yes" #--force-yes"
-		;;
 		*)
-		echo "È stata indicata un'opzione illegale."
-		echo "Utilizzare $0 -h per ulteriori informazioni."
+		echo "Invalid parameter."
+		echo ""
+		$0 -h
 		exit 1
 		;;
 	esac
 done
 
-lsb_release -c | grep -q $DISTRIBUZIONE 
+lsb_release -c | grep -q $CODENAME 
 if [[ $? -ne 0 ]]
 then
-	echo "Questo script è utilizzabile esclusivamente su Ubuntu 14.04 ($DISTRIBUZIONE)"
-	echo "Info sulla tua distribuzione:"
+	echo "This script can be used only with Ubuntu $RELEASE ($CODENAME)"
+	echo ""
+	echo "Distribution-specific information of your current installation:"
 	lsb_release -d -c
 	exit 1
 fi
@@ -83,128 +74,119 @@ fi
 
 if [[ $EUID -ne 0 ]]
 then
-	echo "Lo script richiede l'esecuzione con i permessi di root." 1>&2
+	echo "This script require root permission." 1>&2
 	chmod 755 ./`basename $0`
 	sudo ./`basename $0`
 	exit 0
 fi
 
 function add_repo {
-echo -n "Repository di $1: "
+echo -n "Repository $1: "
 if cat /etc/apt/sources.list /etc/apt/sources.list.d/*.list | grep -q "$2"
 then
-	echo "Già presente"
+	echo "Already added"
 else
 	echo "$2" >> /etc/apt/sources.list.d/$1.list
 	#	echo "$2 #$1" >> /etc/apt/sources.list
 	chmod 644 /etc/apt/sources.list.d/$1.list
-	echo "Aggiunto"
+	echo "Added"
 	if [[ $# -gt 2 ]]
 	then
-		echo -n "Importazione chiave di $1: "
-		 wget -q -O - $3 | apt-key add -
+		echo -n "Adding trusted key for $1: "
+		wget -q -O - $3 | apt-key add -
 		sleep 2
 	fi
 fi
 }
 
 function enable_repo {
-echo "poba = $3"
 if cat /etc/apt/sources.list | grep -q "# $2"
 then
-	echo -n "Abilitazione Repository $1: "
+	echo -n "Enabling repository $1: "
 	if eval $3
 	then
 		echo "OK"
 	else
-		echo "FALLITA!"
+		echo "FAIL!"
 	fi
 else
 	echo -n "Repository $1: "
 	if cat /etc/apt/sources.list | grep -q "$2"
 	then
-		echo "Già presente"
+		echo "Already enabled"
 	else
 		echo "$2 #$1" >> /etc/apt/sources.list
-		echo "Aggiunto"
+		echo "Enabled"
 	fi
 fi
 
 }
 
+PARTNER="deb http://archive.canonical.com/ubuntu $CODENAME partner"
+PARTNER_SRC="deb-src http://archive.canonical.com/ubuntu $CODENAME partner"
+PARTNER_SED='sed -i -e "s/# deb http:\/\/archive.canonical.com\/ubuntu '$CODENAME' partner/deb http:\/\/archive.canonical.com\/ubuntu '$CODENAME' partner/g" /etc/apt/sources.list'
+PARTNER_SRC_SED='sed -i -e "s/# deb-src http:\/\/archive.canonical.com\/ubuntu '$CODENAME' partner/deb-src http:\/\/archive.canonical.com\/ubuntu '$CODENAME' partner/g" /etc/apt/sources.list'
 
-REPOLIST="JDownloader, Synapse, Faenza, X-Updates, Google Chrome, Google Talk"
-
-PINGSITE="www.google.com"
-
-PARTNER="deb http://archive.canonical.com/ubuntu $DISTRIBUZIONE partner"
-PARTNER_SRC="deb-src http://archive.canonical.com/ubuntu $DISTRIBUZIONE partner"
-PARTNER_SED='sed -i -e "s/# deb http:\/\/archive.canonical.com\/ubuntu '$DISTRIBUZIONE' partner/deb http:\/\/archive.canonical.com\/ubuntu '$DISTRIBUZIONE' partner/g" /etc/apt/sources.list'
-PARTNER_SRC_SED='sed -i -e "s/# deb-src http:\/\/archive.canonical.com\/ubuntu '$DISTRIBUZIONE' partner/deb-src http:\/\/archive.canonical.com\/ubuntu '$DISTRIBUZIONE' partner/g" /etc/apt/sources.list'
-
-BACKPORTS="deb http://it.archive.ubuntu.com/ubuntu/ $DISTRIBUZIONE-backports main restricted universe multiverse"
-BACKPORTS_SRC="deb-src http://it.archive.ubuntu.com/ubuntu/ $DISTRIBUZIONE-backports main restricted universe multiverse"
-BACKPORTS_SED='sed -i -e "s/# deb http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$DISTRIBUZIONE'-backports main restricted universe multiverse/deb http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$DISTRIBUZIONE'-backports main restricted universe multiverse/g" /etc/apt/sources.list'
-BACKPORTS_SRC_SED='sed -i -e "s/# deb-src http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$DISTRIBUZIONE'-backports main restricted universe multiverse/deb-src http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$DISTRIBUZIONE'-backports main restricted universe multiverse/g" /etc/apt/sources.list'
+BACKPORTS="deb http://it.archive.ubuntu.com/ubuntu/ $CODENAME-backports main restricted universe multiverse"
+BACKPORTS_SRC="deb-src http://it.archive.ubuntu.com/ubuntu/ $CODENAME-backports main restricted universe multiverse"
+BACKPORTS_SED='sed -i -e "s/# deb http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$CODENAME'-backports main restricted universe multiverse/deb http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$CODENAME'-backports main restricted universe multiverse/g" /etc/apt/sources.list'
+BACKPORTS_SRC_SED='sed -i -e "s/# deb-src http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$CODENAME'-backports main restricted universe multiverse/deb-src http:\/\/it.archive.ubuntu.com\/ubuntu\/ '$CODENAME'-backports main restricted universe multiverse/g" /etc/apt/sources.list'
 
 
 VIDEOLAN="deb http://download.videolan.org/pub/debian/stable/ /"
 VIDEOLAN_KEY="http://download.videolan.org/pub/debian/videolan-apt.asc"
 SKYPE="deb http://download.skype.com/linux/repos/debian stable non-free"
-DROPBOX="deb http://linux.dropbox.com/ubuntu $DISTRIBUZIONE main"
-DROPBOX_SRC="deb-src http://linux.dropbox.com/ubuntu $DISTRIBUZIONE main"
-VIRTUALBOX="deb http://download.virtualbox.org/virtualbox/debian $DISTRIBUZIONE contrib"
+DROPBOX="deb http://linux.dropbox.com/ubuntu $CODENAME main"
+DROPBOX_SRC="deb-src http://linux.dropbox.com/ubuntu $CODENAME main"
+VIRTUALBOX="deb http://download.virtualbox.org/virtualbox/debian $CODENAME contrib"
 VIRTUALBOX_KEY="http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc"
 #VBOX_USB="none /proc/bus/usb usbfs devgid=125,devmode=664 0 0"
 GOOGLE_CHROME="deb http://dl.google.com/linux/chrome/deb/ stable main"
 GOOGLE_MUSIC_MANAGER="deb http://dl.google.com/linux/musicmanager/deb/ stable main"
 GOOGLE_TALKPLUGIN="deb http://dl.google.com/linux/talkplugin/deb/ stable main"
 GOOGLE_KEY="https://dl-ssl.google.com/linux/linux_signing_key.pub"
-GETDEB="deb http://archive.getdeb.net/ubuntu $DISTRIBUZIONE-getdeb apps"
+GETDEB="deb http://archive.getdeb.net/ubuntu $CODENAME-getdeb apps"
 GETDEB_KEY="http://archive.getdeb.net/getdeb-archive.key"
 
-echo "Questo script aggiungerà al source.list i seguenti repository per Ubuntu 12.04:"
-echo "$REPOLIST"
-echo
-echo "Provvederà poi ad installare i seguenti pacchetti:"
+echo "List of packages to be installed:"
 echo "$APPZ"
 echo
-read -p "Continuare [S/n]? " sn
+read -p "Continuae [Y/n]? " yn
 
-if [[ $sn != 's' ]] && [[ $sn != 'S' ]] && [[ $sn != '' ]]
+if [[ $yn != 'y' ]] && [[ $yn != 'Y' ]] && [[ $yn != '' ]]
 then
-	echo "Interrotto."
+	echo "Installation canceled"
 	exit 1
 fi
 
 echo
-echo -n "Verifica connessione ad internet: "
+echo -n "Checking internet connection: "
 ping -c1 $PINGSITE > /dev/null
 if [ "$?" -ne 0 ] 
 then
-	echo "FALLITA"
-	echo "Il ping di $PINGSITE non è riuscito."
-	echo "Controllare la connessione ad internet e riprovare"
+	echo "FAIL"
+	echo "Unable to ping $PINGSITE."
+	echo "Check your internet connection and try again."
 	exit 1
 fi
 echo "OK"
 
 echo
-echo "=== Aggiornamento sorgenti software ==="
+echo "=== Updating package indexes ==="
 echo
 sleep 1
 apt-get update
 echo
-echo "=== Aggiornamento pacchetti installati ==="
+echo "=== Updating installed packages ==="
 echo
 sleep 1
 apt-get upgrade $AUTOYES
 
 echo
-echo "=== Aggiunta repository ==="
+echo "=== Adding repositories ==="
 sleep 1
 
-echo -n "Backup del source.list originare: "
+echo -n "Backup original source.list: "
 if cp /etc/apt/sources.list /etc/apt/sources.list.`date +%Y%m%d-%H%M%S`
 then
 	echo "OK"
@@ -212,9 +194,9 @@ fi
 echo
 
 enable_repo "Ubuntu Partner" "$PARTNER" "$PARTNER_SED"
-enable_repo "Ubuntu Partner (Sorgenti)" "$PARTNER_SRC" "$PARTNER_SRC_SED"
-enable_repo "Ubuntu $DISTRIBUZIONE-Backports" "$BACKPORTS" "$BACKPORTS_SED"
-enable_repo "Ubuntu $DISTRIBUZIONE-Backports (Sorgenti)" "$BACKPORTS_SRC" "$BACKPORTS_SRC_SED"
+enable_repo "Ubuntu Partner (Sources)" "$PARTNER_SRC" "$PARTNER_SRC_SED"
+enable_repo "Ubuntu $CODENAME-Backports" "$BACKPORTS" "$BACKPORTS_SED"
+enable_repo "Ubuntu $CODENAME-Backports (Sources)" "$BACKPORTS_SRC" "$BACKPORTS_SRC_SED"
 
 #add-apt-repository -y ppa:tualatrix/ppa #Ubuntu-Tweak
 #add-apt-repository -y ppa:jd-team/jdownloader #JDownloader
@@ -240,44 +222,32 @@ add_repo "google-chrome-beta" "$GOOGLE_CHROME" "$GOOGLE_KEY"
 #add_repo "getdeb" "$GETDEB" "$GETDEB_KEY"
 #add_repo "skype" "$SKYPE"
 
-echo "=== Repository aggiunti ==="
+echo "=== Repositories added ==="
 echo
 
 sleep 2
 
-echo "L'esecuzione proseguirà con l'installazione dei seguenti paccehtti:"
-echo "$APPZ"
 echo
-read -p "Continuare [S/n]? " sn
-
-if [[ $sn != 's' ]] && [[ $sn != 'S' ]] && [[ $sn != '' ]]
-then
-	echo "Interrotto."
-	exit 1
-fi
-
-echo
-echo "=== Aggiornamento sorgenti software ==="
+echo "=== Updating package indexes ==="
 echo
 sleep 1
 apt-get update
 echo
-echo "=== Aggiornamento pacchetti installati ==="
+echo "=== Updating installed packages ==="
 echo
 sleep 1
 apt-get upgrade $AUTOYES
 echo
-echo "=== Installazione pacchetti ==="
+echo "=== Installing new packages ==="
 echo
 sleep 1
 apt-get install $AUTOYES $APPZ
 apt-get remove $AUTOYES postfix indicator-appmenu
-pip install Glances
-pip install PySensors
+#pip install Glances
 
 
 echo
-echo "=== Disabilitazione accelerazione mouse ==="
+echo "=== Disabling mouse acceleration ==="
 echo
 FILE="/usr/share/X11/xorg.conf.d/50-mouse-acceleration.conf"
 
